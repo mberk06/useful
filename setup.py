ca0e719d0b6354e2a25e839bc54725a725ef794d
Michael Berk <michael.berk@databricks.com>
2023-06-25 21:55:43 -0400

    rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="mypackage",
+    name="useful",
     version="0.1.0",
     url="https://github.com/mberk06/useful",
     author="Michael Berk",
