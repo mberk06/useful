aa52b7ae71f52ddab05d105dcfd8c4b2198199cd
michael-berk <michaelberk99@gmail.com>
2024-01-29 20:21:13 -0500

    Create meausre_time.py

---
+++ useful/meausre_time.py
@@ -0,0 +1,12 @@
+import time
+import functools
+
+def measure_time(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        return result, execution_time
+    return wrapper
